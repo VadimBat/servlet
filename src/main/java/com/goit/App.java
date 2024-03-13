07ee3ef2c009d51f50cceed8f31d2a118582903e
Vadim Batyashov <changcheng39@gmail.com>
2024-03-13 18:00:46 +0400

    Task2

--- src/main/java/com/goit/App.java
+++ src/main/java/com/goit/App.java
@@ -8,11 +8,19 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import static java.util.Calendar.HOUR;
+
 public class App {
     public static void main(String[] args) {
-        Date currentDateTimeUTC = Date.from(Instant.now());
+        Duration duration = Duration.ofHours(2);
+        Date currentDateTimeUTC = Date.from(Instant.now().plus(duration));
+
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
         System.out.println(dateFormat.format(currentDateTimeUTC));
+        String s = "UTC+3";
+        String [] ss = s.split("\\+");
+        System.out.println(Integer.parseInt(ss[ss.length - 1]));
+
     }
 }
--- src/main/java/com/goit/servlet/TimeServlet.java
+++ src/main/java/com/goit/servlet/TimeServlet.java
@@ -9,8 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.time.Duration;
 import java.time.Instant;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -20,21 +20,47 @@ public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-        String currentTimeUTC = dateFormat.format(currentDateTimeUTC);
-
         StringBuilder htmlBuilder = new StringBuilder();
         htmlBuilder.append("<html><body>")
                 .append("<h1>")
-                .append(currentTimeUTC)
-                .append(" UTC")
+                .append(parseCurrentDateTime(req))
+                .append(" UTC ")
+                .append(parseTimezone(req))
                 .append("<h1>")
                 .append("<body><html>");
 
         resp.setContentType("text/html; charset=utf-8");
         PrintWriter out = resp.getWriter();
-        out.print(htmlBuilder.toString());
+        out.print(htmlBuilder);
         out.close();
+    }
+
+    private String parseTimezone(HttpServletRequest req){
+        if (req.getParameterMap().containsKey("timezone")){
+            return "+" + req.getParameter("timezone");
+        }else {
+            return "";
+        }
+    }
+    private String parseCurrentDateTime(HttpServletRequest req){
+        String parameter = req.getParameter("timezone");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        String currentTimeUTC = dateFormat.format(currentDateTimeUTC);
+        if (req.getParameterMap().containsKey("timezone")){
+            int diffHours = 0;
+            if (parameter.length()>1){
+                String[] parametersContent = parameter.split(" ");
+                diffHours = Integer.parseInt(parametersContent[parametersContent.length-1]);
+            } else {
+                diffHours = Integer.parseInt(parameter);
+            }
+            Duration duration = Duration.ofHours(diffHours);
+            Date currentDateTime = Date.from(Instant.now().plus(duration));
+
+            return dateFormat.format(currentDateTime);
+        }else {
+            return currentTimeUTC;
+        }
     }
 }
